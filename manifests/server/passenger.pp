9a8775b3ea817a2674a5e9a7bbc0c51dfa0690f0
Laurent Bigonville <bigon@bigon.be>
2012-03-09 14:26:53 +0100

    Install puppetmaster-passenger module on debian and nothing else

--- manifests/server/passenger.pp
+++ manifests/server/passenger.pp
@@ -2,39 +2,45 @@ class puppet::server::passenger {
   include apache::ssl
   include ::passenger
 
-  file {'puppet_vhost':
-    path    => "${puppet::params::apache_conf_dir}/puppet.conf",
-    content => template('puppet/server/puppet-vhost.conf.erb'),
-    mode    => '0644',
-    notify  => Exec['reload-apache'],
-  }
+  if $::operatingsystem == 'centos' or $::operatingsystem == 'redhat' {
+    file {'puppet_vhost':
+      path    => "${puppet::params::apache_conf_dir}/puppet.conf",
+      content => template('puppet/server/puppet-vhost.conf.erb'),
+      mode    => '0644',
+      notify  => Exec['reload-apache'],
+    }
 
-  exec {'restart_puppet':
-    command     => "/bin/touch ${puppet::params::app_root}/tmp/restart.txt",
-    refreshonly => true,
-    cwd         => $puppet::params::app_root,
-    path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-    require     => [Class['puppet::server::install'],File["${puppet::params::app_root}/tmp"]],
-  }
+    exec {'restart_puppet':
+      command     => "/bin/touch ${puppet::params::app_root}/tmp/restart.txt",
+      refreshonly => true,
+      cwd         => $puppet::params::app_root,
+      path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+      require     => [Class['puppet::server::install'],File["${puppet::params::app_root}/tmp"]],
+    }
 
-  file {
-    [$puppet::params::app_root, "${puppet::params::app_root}/public", "${puppet::params::app_root}/tmp"]:
-      ensure => directory,
-      owner  => $puppet::params::user,
-  }
-  file {
-    "${puppet::params::app_root}/config.ru":
-      owner  => $puppet::params::user,
-      source => 'puppet:///modules/puppet/config.ru',
-      notify => Exec['restart_puppet'],
+    file {
+      [$puppet::params::app_root, "${puppet::params::app_root}/public", "${puppet::params::app_root}/tmp"]:
+        ensure => directory,
+        owner  => $puppet::params::user,
+    }
+    file {
+      "${puppet::params::app_root}/config.ru":
+        owner  => $puppet::params::user,
+        source => 'puppet:///modules/puppet/config.ru',
+        notify => Exec['restart_puppet'],
+    }
   }
 
   if $::operatingsystem == 'debian' or $::operatingsystem == 'Ubuntu' {
     augeas { '/etc/default/puppetmaster':
-      changes => $puppetmaster::frontend ? {
-        default => ["set /files/etc/default/puppetmaster/START yes",],
+      changes     => $puppetmaster::frontend ? {
+        default   => ["set /files/etc/default/puppetmaster/START yes",],
         passenger => ["set /files/etc/default/puppetmaster/START no",],
       },
+      require => Package['puppetmaster'],
+    }
+    package { 'puppetmaster-passenger':
+      ensure => installed,
     }
   }
 }
